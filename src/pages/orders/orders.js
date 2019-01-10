ee24c769b492405acf2282181f5f3a180a72a801
zxj <345624137@qq.com>
2019-01-10 21:55:20 +0800

    地址添加页面和功能实现

--- src/pages/orders/orders.js
+++ src/pages/orders/orders.js
@@ -1,138 +1,203 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View,Text,Picker,Image,Button} from '@tarojs/components'
-import {AtList,AtListItem,AtInput,AtIcon,AtRadio,AtMessage,AtModal, AtModalHeader, AtModalContent, AtModalAction} from 'taro-ui'
-import './orders.less'
-
+import Taro, { Component } from "@tarojs/taro";
+import { View, Text, Picker, Image, Button } from "@tarojs/components";
+import {
+  AtList,
+  AtListItem,
+  AtInput,
+  AtIcon,
+  AtRadio,
+  AtMessage,
+  AtModal,
+  AtModalHeader,
+  AtModalContent,
+  AtModalAction,
+  AtForm
+} from "taro-ui";
+import "./orders.less";
 
 export default class Orders extends Component {
-constructor(){
-    super(...arguments)
-    this.state={
-      shopInfo:{
-        shopName:'都会开始发生回复康师傅',
-        price:99.00,//价格
-        stores:1000,//存货
-        img:'',
-        sales:2//购买数量
+  constructor() {
+    super(...arguments);
+    this.state = {
+      user: "",
+      city: "",
+      phone: "",
+      ems: "",
+      shopInfo: {
+        shopName: "都会开始发生回复康师傅",
+        price: 99.0,
+        stores: 1000,
+        img: "",
+        sales: 2
       },
-      selector: ['普通快递', '顺丰快递', '申通快递', 'EMS快递','中通快递','圆通快递'],
-      deliveryCosts:[10,20,10,15,10,10],//快递费
-      selectorChecked: '普通快递',
-      checkCosts:10,//选择的快递费
-      message:'',
-      adderss:'',
-      allMoney:0,
-      isOpened:false
-    }
+      selector: [
+        "普通快递",
+        "顺丰快递",
+        "申通快递",
+        "EMS快递",
+        "中通快递",
+        "圆通快递"
+      ],
+      deliveryCosts: [10, 20, 10, 15, 10, 10],
+      selectorChecked: "普通快递",
+      checkCosts: 10,
+      message: "",
+      adderss: "",
+      adderssLists: [
+        {
+          label:
+            "湖南省永州市冷水滩区中央新城C10-1702 联系人：赵学军 联系方式：18520820070",
+          value:
+            "湖南省永州市冷水滩区中央新城C10-1702 联系人：赵学军 联系方式：18520820070"
+        }
+      ],
+      allMoney: 0,
+      isOpened: false
+    }; //价格 //存货 //购买数量 //快递费 //选择的快递费
   }
 
-  onChange = e => {
-    console.log(e.detail.value)
-      this.setState({
-        selectorChecked: this.state.selector[e.detail.value],
-        checkCosts: this.state.deliveryCosts[e.detail.value]
-      })
-  }
-  
- addAdderss=(e)=>{
-   //添加地址
-   this.setState({
-     isOpened:true
-   })
- }
+  onSelectChange = e => {
+    console.log(e.detail.value);
+    this.setState({
+      selectorChecked: this.state.selector[e.detail.value],
+      checkCosts: this.state.deliveryCosts[e.detail.value]
+    });
+  };
+
+  addAdderss = e => {
+    //打开添加地址
+    this.setState({ isOpened: true });
+  };
+
+  closeAdd = e => {
+    //关闭添加地址
+    this.setState({ isOpened: false });
+  };
 
   /**
    * 返回首页
    */
-  goHome=(e)=>{
+  goHome = e => {
     Taro.navigateTo({
-      url:`../../pages/index/index`
-    })
+      url: `../../pages/index/index`
+    });
+  };
+
+  nextDo = e => {};
+
+  handleChange = value => {
+    //留言
+    this.setState({ message: value });
+  };
+
+  handleRadioChange(value) {
+    //地址选择
+    console.log(value);
+    this.setState({ adderss: value });
   }
 
-  nextDo=(e)=>{
-
-  }
-
-  handleChange=(value)=>{//留言
+  handleNameChange = e => {
+    //地址添加
+    this.setState({ user: e });
+  };
+  handleTelChange = e => {
+    //地址添加
+    this.setState({ phone: e });
+  };
+  handleCityChange = e => {
+    //地址添加
+    this.setState({ city: e });
+  };
+  handleEmsChange = e => {
+    //地址添加
+    this.setState({ ems: e });
+  };
+  submitAddChange = e => {
+    let addCity = `地址:${this.state.city};收件人:${this.state.user};联系方式:${
+      this.state.phone
+    };邮政编码:${this.state.ems};`;
+    console.log(addCity);
+    let addItem = { label: addCity, value: addCity };
+    let newCity = [addItem, ...this.state.adderssLists];
     this.setState({
-      message:value
-    })
-  }
+      user: "",
+      city: "",
+      phone: "",
+      ems: "",
+      isOpened: false,
+      adderssLists: newCity
+    });
+  };
 
-  handleRadioChange (value) {//地址选择
-    console.log(value)
-    this.setState({
-      adderss:value
-    })
-  }
+  submitForm = e => {
+    //答案提交
 
-  submitForm=(e)=>{//答案提交
-   
-    let ordersInfo={}
-    if(this.state.adderss==''){
-      Taro.atMessage({//type为：success,error,warning
-        'message': '收件人地址必填!',
-        'type': 'error',
-      })
-    }else{
-      ordersInfo['price']=this.state.shopInfo.price
-      ordersInfo['shopName']=this.state.shopInfo.shopName
-      ordersInfo['sales']=this.state.shopInfo.sales
-      ordersInfo["adderss"]=this.state.adderss
-      ordersInfo['message']=this.state.message
-      ordersInfo['selectorChecked']=this.state.selectorChecked
-      ordersInfo['checkCosts']=this.state.checkCosts
-      ordersInfo['allMoney']=this.state.allMoney
+    let ordersInfo = {};
+    if (this.state.adderss == "") {
+      Taro.atMessage({
+        //type为：success,error,warning
+        message: "收件人地址必填!",
+        type: "error"
+      });
+    } else {
+      ordersInfo["price"] = this.state.shopInfo.price;
+      ordersInfo["shopName"] = this.state.shopInfo.shopName;
+      ordersInfo["sales"] = this.state.shopInfo.sales;
+      ordersInfo["adderss"] = this.state.adderss;
+      ordersInfo["message"] = this.state.message;
+      ordersInfo["selectorChecked"] = this.state.selectorChecked;
+      ordersInfo["checkCosts"] = this.state.checkCosts;
+      ordersInfo["allMoney"] = this.state.allMoney;
     }
-    console.log(ordersInfo)
-  }
+    console.log(ordersInfo);
+  };
 
+  config = { navigationBarTitleText: "确定订单" };
 
-  config = {
-    navigationBarTitleText: '确定订单'
-  }
+  componentWillMount() {}
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentDidMount () { }
+  componentWillUnmount() {}
 
-  componentWillUnmount () { }
+  componentDidShow() {}
 
-  componentDidShow () { }
+  componentDidHide() {}
 
-  componentDidHide () { }
-
-  render () {
-    let {shopInfo}=this.state
-    let shopMoney=shopInfo.sales*shopInfo.price
-    this.state.allMoney=shopMoney+ this.state.checkCosts
+  render() {
+    let { shopInfo } = this.state;
+    let shopMoney = shopInfo.sales * shopInfo.price;
+    this.state.allMoney = shopMoney + this.state.checkCosts;
     return (
       <View className='order-mode'>
         <AtMessage />
         <View className='adderss'>
           <AtList>
-            <AtListItem onClick={this.addAdderss.bind(this)} title='添加地址' arrow='right' />
+            <AtListItem
+              onClick={this.addAdderss.bind(this)}
+              title='添加地址'
+              arrow='right'
+            />
           </AtList>
           <View className='check-info'>
             <AtRadio
-              options={[
-          { label: '湖南省永州市冷水滩区中央新城C10-1702 联系人：赵学军 联系方式：18520820070', value: '湖南省永州市冷水滩区中央新城C10-1702 联系人：赵学军 联系方式：18520820070'},
-          { label: '单选项二', value: 'option2' },
-        ]}
+              options={this.state.adderssLists}
               value={this.state.adderss}
               onClick={this.handleRadioChange.bind(this)}
-            /> 
+            />
           </View>
         </View>
 
         <View className='order-info'>
           <View className='shop-name'>
-            <AtIcon value='menu' size='20'></AtIcon>
+            <AtIcon value='menu' size='20' />
             <Text className='name'>深圳双创小店</Text>
           </View>
           <View className='shop-info'>
-            <Image src={require('../../assets/imgs/banner_01.jpg')} className='img'></Image>
+            <Image
+              src={require("../../assets/imgs/banner_01.jpg")}
+              className='img'
+            />
             <View className='shop-title'>{shopInfo.shopName}</View>
             <View className='price-num'>
               <Text className='price'>￥{shopInfo.price}</Text>
@@ -147,24 +212,26 @@ constructor(){
         </View>
 
         <View className='magges'>
-            <View>
-              <Picker mode='selector' range={this.state.selector} onChange={this.onChange}>
-                <View className='picker'>
-                  <Text>
-                   配送方式：
-                  </Text>
-                  <Text>{this.state.selectorChecked}</Text>
-                </View>
-              </Picker>
-            </View>
-            <AtInput
-              name='message'
-              title='买家留言'
-              type='text'
-              placeholder='给商家留言'
-              value={this.state.message}
-              onChange={this.handleChange.bind(this)}
-            />
+          <View>
+            <Picker
+              mode='selector'
+              range={this.state.selector}
+              onChange={this.onSelectChange.bind(this)}
+            >
+              <View className='picker'>
+                <Text>配送方式：</Text>
+                <Text>{this.state.selectorChecked}</Text>
+              </View>
+            </Picker>
+          </View>
+          <AtInput
+            name='message'
+            title='买家留言'
+            type='text'
+            placeholder='给商家留言'
+            value={this.state.message}
+            onChange={this.handleChange.bind(this)}
+          />
         </View>
 
         <View className='menoy'>
@@ -179,20 +246,57 @@ constructor(){
           <View className='menoy-all'>
             <Text>合计:</Text>
             <Text className='money-text'>￥{this.state.allMoney}</Text>
-            <Text className='submit' onClick={this.submitForm.bind(this)}>提交订单</Text>
+            <Text className='submit' onClick={this.submitForm.bind(this)}>
+              提交订单
+            </Text>
           </View>
         </View>
 
         <AtModal isOpened={this.state.isOpened}>
-          <AtModalHeader>标题</AtModalHeader>
+          <AtModalHeader>添加收件地址</AtModalHeader>
           <AtModalContent>
-            这里是正文内容，欢迎加入京东凹凸实验室
-            这里是正文内容，欢迎加入京东凹凸实验室
-            这里是正文内容，欢迎加入京东凹凸实验室
+            <AtForm>
+              <AtInput
+                name='user'
+                title='收件人'
+                type='text'
+                placeholder='收件人名称'
+                value={this.state.user}
+                onChange={this.handleNameChange.bind(this)}
+              />
+              <AtInput
+                name='city'
+                title='收件人地址'
+                type='text'
+                placeholder='收件人地址'
+                value={this.state.city}
+                onChange={this.handleCityChange.bind(this)}
+              />
+              <AtInput
+                name='phone'
+                title='联系电话'
+                type='phone'
+                placeholder='手机号码'
+                value={this.state.phone}
+                onChange={this.handleTelChange.bind(this)}
+              />
+              <AtInput
+                name='ems'
+                title='邮政编码'
+                type='number'
+                placeholder='请输入邮政编码'
+                value={this.state.ems}
+                onChange={this.handleEmsChange.bind(this)}
+              />
+            </AtForm>
           </AtModalContent>
-          <AtModalAction> <Button>取消</Button> <Button>确定</Button> </AtModalAction>
+          <AtModalAction>
+            {" "}
+            <Button onClick={this.closeAdd.bind(this)}>取消</Button>{" "}
+            <Button onClick={this.submitAddChange}>添加</Button>{" "}
+          </AtModalAction>
         </AtModal>
       </View>
-    )
+    );
   }
 }
